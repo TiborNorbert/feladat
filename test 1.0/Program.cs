2d317ce1484ab61fc3ce9a43b72ddfea611c036f
TiborNorbert <112852931+TiborNorbert@users.noreply.github.com>
2022-09-21 12:53:59 +0200

    2.0

---
+++ test 1.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adj be 2-t számot te");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
